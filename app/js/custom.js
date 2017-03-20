cfe202b6e12c7481694a238b26f0c45b4333268b
VladZelenko <Vlad6555@ukr.net>
2017-03-20 22:20:42 +0200

    add header

---
+++ app/js/custom.js
@@ -0,0 +1,26 @@
+//випадшка по горизонталі, працює при закоменченому по вертикалі
+function openNav(){
+	document.getElementById('nav').style.width = "100%";
+	document.getElementById('open-btn-nav').style.opacity = "0";
+}
+function closeNav(){
+	document.getElementById('nav').style.width = "0%";
+	document.getElementById('open-btn-nav').style.opacity = "1";
+}
+//випадашка по вертикалі, працює при закоменченому по горизонталі
+// function openNav(){
+// 	document.getElementById('nav').style.height = "100%";
+//	document.getElementById('open-btn-nav').style.opacity = "0";
+// }
+// function closeNav(){
+// 	document.getElementById('nav').style.height = "0%";
+//	document.getElementById('open-btn-nav').style.opacity = "1";
+// }
+
+//slick-carousel
+$('.head_carousel').slick({
+	infinite: true,
+	speed: 1000,
+	autoplaySpeed: 2000,
+	autoplay: true
+});
