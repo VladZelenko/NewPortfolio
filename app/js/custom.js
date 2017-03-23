b54ca0f4fe59007b16c93c2d85ff3a9660540495
VladZelenko <Vlad6555@ukr.net>
2017-03-23 13:17:58 +0200

    add smooth anim on anchor

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,4 @@
-//випадшка по горизонталі, працює при закоменченому по вертикалі
+//mobile menu
 function openNav(){
 	document.getElementById('nav').style.width = "100%";
 	document.getElementById('open-btn-nav').style.opacity = "0";
@@ -7,12 +7,12 @@ function closeNav(){
 	document.getElementById('nav').style.width = "0%";
 	document.getElementById('open-btn-nav').style.opacity = "1";
 }
-//випадашка по вертикалі, працює при закоменченому по горизонталі
-// function openNav(){
-// 	document.getElementById('nav').style.height = "100%";
-//	document.getElementById('open-btn-nav').style.opacity = "0";
-// }
-// function closeNav(){
-// 	document.getElementById('nav').style.height = "0%";
-//	document.getElementById('open-btn-nav').style.opacity = "1";
-// }
+//smooth transition to an anchor
+$(document).ready(function() {
+	$('a[href^="#"]').click(function(){
+		var el = $(this).attr('href');
+		$('body').animate({
+			scrollTop: $(el).offset().top}, 1500);
+		return closeNav();
+	});
+});
